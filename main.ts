e08a25557b724f75c12c3d8e3dfe1b655f9a8b6e
Idrees Hassan <idrees@idreesinc.com>
2022-03-09 19:50:07 -0500

    Add automatic waypoint generation

--- main.ts
+++ main.ts
@@ -1,85 +1,144 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TAbstractFile, TFile, TFolder } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface WaypointSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: WaypointSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Waypoint extends Plugin {
+	static readonly WAYPOINT_FLAG = "%% Waypoint %%";
+	static readonly BEGIN_WAYPOINT = "%% Begin Waypoint %%";
+	static readonly END_WAYPOINT = "%% End Waypoint %%";
+
+	settings: WaypointSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.registerEvent(this.app.vault.on("modify", this.detectWaypointFlag));
+		this.app.vault.getFiles
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
+	}
 
+	detectWaypointFlag = async (file: TFile) => {
+		console.log("Scanning for Waypoint flags...");
+		const text = await this.app.vault.cachedRead(file);
+		const lines: string[] = text.split("\n");
+		for (let i = 0; i < lines.length; i++) {
+			if (lines[i].trim() === (Waypoint.WAYPOINT_FLAG)) {
+				console.log("Found waypoint flag!");
+				await this.updateWaypoint(file);
+				await this.updateParentWaypoint(file);
+				return;
+			}
+		}
+		console.log("No waypoint flags found.");
+	}
+
+	async updateWaypoint(file: TFile, ) {
+		console.log("Updating waypoint in " + file.path);
+		const fileTree = await this.getFileTreeRepresentation(file.parent, 0, true);
+		const waypoint = `${Waypoint.BEGIN_WAYPOINT}\n${fileTree}\n${Waypoint.END_WAYPOINT}`;
+		console.log(fileTree);
+		const text = await this.app.vault.read(file);
+		const lines: string[] = text.split("\n");
+		let waypointStart = -1;
+		let waypointEnd = -1;
+		for (let i = 0; i < lines.length; i++) {
+			const trimmed = lines[i].trim();
+			if (waypointStart === -1 && (trimmed === (Waypoint.WAYPOINT_FLAG) || trimmed === (Waypoint.BEGIN_WAYPOINT))) {
+				waypointStart = i;
+			} else if (waypointStart !== -1 && trimmed === (Waypoint.END_WAYPOINT)) {
+				waypointEnd = i;
+				break;
+			}
+		}
+		if (waypointStart === -1) {
+			console.error("Error: No waypoint found while trying to update " + file.path);
+			return;
+		}
+		console.log("Waypoint found at " + waypointStart + " to " + waypointEnd);
+		lines.splice(waypointStart, waypointEnd !== -1 ? waypointEnd - waypointStart + 1 : 1, waypoint);
+		console.log(lines.join("\n"));
+		await this.app.vault.modify(file, lines.join("\n"));
+	}
+
+	async getFileTreeRepresentation(node: TAbstractFile, indentLevel: number, topLevel = false): Promise<string>|null {
+		const bullet = "	".repeat(indentLevel) + "-";
+		if (node instanceof TFile) {
+			return `${bullet} [[${node.basename}]]`;
+		} else if (node instanceof TFolder) {
+			if (!topLevel) {
+				const folderNote = this.app.vault.getAbstractFileByPath(node.path + "/" + node.name + ".md");
+				if (folderNote instanceof TFile) {
+					const content = await this.app.vault.cachedRead(folderNote);
+					if (content.includes(Waypoint.BEGIN_WAYPOINT) || content.includes(Waypoint.WAYPOINT_FLAG)) {
+						return `${bullet} **[[${folderNote.basename}]]**`;
+					}
+				}
+			}
+			let text = `${bullet} **${node.name}**`;
+			if (node.children && node.children.length > 0) {
+				let children = node.children;
+				children = children.sort((a, b) => {
+					const aName = a.name.toLowerCase();
+					const bName = b.name.toLowerCase();
+					if (aName < bName) {
+						return -1;
+					} else if (aName > bName) {
+						return 1;
+					}
+					return 0;
+				});
+				text += "\n" + (await Promise.all(children.map(child => this.getFileTreeRepresentation(child, indentLevel + 1))))
+					.filter(Boolean)
+					.join("\n");
+				return text;
+			} else {
+				return `${bullet} **${node.name}**`;
+			}
+
+		}
+		return null;
+	}
+
+	async updateParentWaypoint(file: TFile) {
+		const parentWaypoint = await this.locateParentWaypoint(file);
+		if (parentWaypoint !== null) {
+			this.updateWaypoint(parentWaypoint);
+		}
+	}
+
+	async locateParentWaypoint(file: TFile): Promise<TFile> {
+		console.log("Locating parent waypoint...");
+		if (file.parent) {
+			let folder = file.parent;
+			while (folder.parent) {
+				folder = folder.parent;
+				console.log(folder.name);
+				const folderNote = this.app.vault.getAbstractFileByPath(folder.path + "/" + folder.name + ".md");
+				if (folderNote instanceof TFile) {
+					console.log("Found folder note: " + folderNote.path);
+					const text = await this.app.vault.cachedRead(folderNote);
+					if (text.includes(Waypoint.BEGIN_WAYPOINT) || text.includes(Waypoint.WAYPOINT_FLAG)) {
+						console.log("Found parent waypoint!");
+						return folderNote;
+					}
+				}
+			}
+		}
+		console.log("No parent waypoint found.");
+		return null;
 	}
 
 	async loadSettings() {
@@ -91,26 +150,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: Waypoint;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Waypoint) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
